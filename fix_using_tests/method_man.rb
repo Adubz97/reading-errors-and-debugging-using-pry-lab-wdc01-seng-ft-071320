fda4765403c207c4446c9476f3326a5446733ddd
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-05-29 01:27:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,9 +4,9 @@
 
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  p "Hello #{player1} & #{playar2}"
 end
 
-def play_game(player2)
+def play_game(player1,player2)
   "#{player1} is better than #{player2}"
 end
